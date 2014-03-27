f86bc1a974d50b36f158f73474476d01ddcef659
RuedigerMoeller <moru0011@gmail.com>
2014-03-27 01:47:15 +0100

    refactoring

---
+++ src/main/java/de/ruedigermoeller/serialization/FSTEncoder.java
@@ -0,0 +1,69 @@
+package de.ruedigermoeller.serialization;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Created by ruedi on 27.03.14.
+ */
+public interface FSTEncoder {
+
+    /**
+     * does not write length, just plain array
+     */
+    void writeFBooleanArr(boolean[] arr) throws IOException;
+    void writeFLongArr(long[] arr) throws IOException;
+    void writeFFloatArr(float[] arr) throws IOException;
+    void writeFDoubleArr(double[] arr) throws IOException;
+    void writeFShortArr(short[] arr) throws IOException;
+    void writeFCharArr(char[] arr) throws IOException;
+    void writeFByteArr(byte[] array) throws IOException;
+    void writeFByteArr(byte[] array, int start, int length) throws IOException;
+
+    void writeStringUTF(String str) throws IOException;
+
+    void writeFShort(short c) throws IOException;
+    void writeFChar(char c) throws IOException;
+    void writeFByte(int v) throws IOException;
+    void writeFIntArr(int v[]) throws IOException;
+    void writeFInt(int anInt) throws IOException;
+    void writeFLong(long anInt) throws IOException;
+    void writeFFloat(float value) throws IOException;
+    void writeFDouble(double value) throws IOException;
+
+    int getWritten();
+    void setWritten(int written);
+    void skip(int i);
+
+    /**
+     * close and flush to underlying stream if present. The stream is also closed
+     * @throws java.io.IOException
+     */
+    void close() throws IOException;
+    void reset();
+    void reset(byte[] out);
+
+    /**
+     * resets stream (positions are lost)
+     * @throws java.io.IOException
+     */
+    void flush() throws IOException;
+
+    /**
+     * used to write uncompressed int (guaranteed length = 4) at a (eventually recent) position
+     * @param position
+     * @param v
+     */
+    void writeInt32At(int position, int v);
+
+    /**
+     * if output stream is null, just encode into a byte array
+     * @param outstream
+     */
+    void setOutstream(OutputStream outstream);
+
+    void ensureFree(int bytes) throws IOException;
+
+    byte[] getBuffer();
+
+}
