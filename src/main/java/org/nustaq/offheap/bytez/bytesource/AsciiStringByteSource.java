76ab428fd7c1938d54e89e9a48e61350621c098e
RuedigerMoeller <moru0011@gmail.com>
2014-08-20 16:17:56 +0200

    undone: "fix: crash in case key contains non ascii char" - false alarm

--- src/main/java/org/nustaq/offheap/bytez/bytesource/AsciiStringByteSource.java
+++ src/main/java/org/nustaq/offheap/bytez/bytesource/AsciiStringByteSource.java
@@ -31,7 +31,7 @@ public class AsciiStringByteSource implements ByteSource {
 
     @Override
     public byte get(long index) {
-        return (byte) (string.charAt(((int) (index + off))) & 0x7f);
+        return (byte) string.charAt(((int) (index + off)));
     }
 
     @Override
