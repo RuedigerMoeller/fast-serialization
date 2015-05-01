7ac1dbbbbfe78af7bc59300f753ba311faec5678
RuedigerMoeller <moru0011@gmail.com>
2015-05-01 21:59:32 +0200

    .

---
+++ src/test/ser/Github61.java
@@ -0,0 +1,50 @@
+package ser;
+
+import org.nustaq.serialization.FSTObjectOutput;
+
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by ruedi on 14.04.2015.
+ */
+public class Github61 {
+
+    /** Size of primitive array */
+    static final int SIZE = 100_000_000;
+
+    public static void main(String[] args) throws Exception {
+
+        CountingOutputStream count;
+        long start, elapsed;
+
+        Object obj = new double[SIZE];
+
+        while ( true ) {
+
+            count = new CountingOutputStream();
+            start = System.nanoTime();
+            try(ObjectOutputStream oos = new ObjectOutputStream(count)) {
+                oos.writeObject(obj);
+            }
+            elapsed = System.nanoTime() - start;
+            System.out.println("STD " + count.count + " bytes written in " + (elapsed)/1000000L + "ms");
+
+            count = new CountingOutputStream();
+            start = System.nanoTime();
+            try(FSTObjectOutput fos = new FSTObjectOutput(count)) {
+                fos.writeObject(obj);
+            }
+            elapsed = System.nanoTime() - start;
+            System.out.println("FST " + count.count + " bytes written in " + (elapsed)/1000000L + "ms");
+        }
+    }
+
+
+    public static class CountingOutputStream extends OutputStream {
+        public long count = 0;
+        @Override public void write(int b) { count++; }
+    }
+
+
+}
