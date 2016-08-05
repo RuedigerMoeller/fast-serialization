2598e97035a75dddd26404f866657dfa27dfce10
RuedigerMoeller <moru0011@gmail.com>
2016-08-05 20:06:44 +0200

    2.48

--- release.sh
+++ release.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
 export PW=$1
+
 echo $1
 
 mvn clean package -Dmaven.test.skip=true gpg:sign -Dgpg.passphrase=$PW
-/opt/gradle/latest/bin/gradle clean release
+cd target
+rm *-jar-with*
+rm -r */
+jar -cf bundle.jar *
+cd ..
 
+gradle release
