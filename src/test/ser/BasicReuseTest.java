12eff983d7241abf4164bfdda4a0178e5f67c5e1
Carter Kozak <ckozak@palantir.com>
2017-06-30 11:39:08 -0400

    Avoid re-reading data from a cached buffer
    Attempting to read an empty stream previously resulted in
    readObject returning the last object the FSTObjectInput read.

---
+++ src/test/ser/BasicReuseTest.java
@@ -0,0 +1,42 @@
+package ser;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.nustaq.serialization.FSTConfiguration;
+import org.nustaq.serialization.FSTObjectInput;
+import org.nustaq.serialization.FSTObjectOutput;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class BasicReuseTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void testStreamReuse() throws Exception {
+        FSTConfiguration configuration = FSTConfiguration.createDefaultConfiguration();
+
+        String expected = "Hello, World!";
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        FSTObjectOutput fstObjectOutput = configuration.getObjectOutput(baos);
+        try {
+            fstObjectOutput.writeObject(expected);
+        } finally {
+            fstObjectOutput.flush();
+        }
+        byte[] serializedData = baos.toByteArray();
+        FSTObjectInput input = configuration.getObjectInput(new ByteArrayInputStream(serializedData));
+        Object read = input.readObject();
+        Assert.assertEquals(expected, read);
+
+        FSTObjectInput secondInput = configuration.getObjectInput(new ByteArrayInputStream(new byte[0]));
+        expectedException.expect(IOException.class);
+        expectedException.expectMessage("Failed to read");
+        secondInput.readObject();
+    }
+}
