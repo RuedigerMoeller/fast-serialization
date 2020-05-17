61d44cfa5919d24539344b614c0977f5284819c8
Rüdiger Möller <moru0011@gmail.com>
2020-05-17 16:13:57 +0200

    3.0.0 release adaption to jdk 14, mem api

--- release.sh
+++ release.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 export PW=$1
+export GPG_TTY=$(tty)
 
 echo $1
 
 mvn clean package -Dmaven.test.skip=true gpg:sign -Dgpg.passphrase=$PW
 cd target
 rm *-jar-with*
+rm *stale-data.txt
 rm -r */
 jar -cf bundle.jar *
 cd ..
-
-gradle release
